8aa1f67d03c6a1d90d32e2a6f56fe9f9abf920c4
Joyankush <joyankushroy@gmail.com>
2026-04-17 16:13:23 +0530

    add global error handler

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -10,9 +10,9 @@ const cloudinary = require('cloudinary').v2;
 // CLOUDINARY_API_KEY=xxx
 // CLOUDINARY_API_SECRET=xxx
 cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME,
-  api_key:    process.env.API_KEY,
-  api_secret: process.env.API_SECRET,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key:    process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 module.exports = cloudinary;
